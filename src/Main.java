768beb3e39b11edce2840adc4a31cda25145e534
andres <saldaandres@hotmail.es>
2026-03-28 20:11:24 -0400

    Se crea el menu del usuario

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     static void main(String[] args) {
-        ArbolInventario arbol= new ArbolInventario();
-        arbol.registrarProducto(5, "telefono");
-        arbol.registrarProducto(6, "televisor");
-        arbol.registrarProducto(3, "reloj");
-        arbol.recorrerEnOrden();
-        System.out.println("parar antes de esto");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n              Bienvenido al Sistema de Control de Inventario");
+        int eleccion = 1;
+        while (eleccion != 0) {
+            imprimirMenu();
+            System.out.print("\nDigita tu eleccion aqui: ");
+            eleccion = scanner.nextInt();
+        }
+    }
+
+    public static void imprimirMenu() {
+        System.out.println("------- Escoge una de las opciones a continuacion -------");
+        System.out.println("|         1. Registrar nuevo producto.                    |");
+        System.out.println("|         2. Mostrar el inventario.                       |");
+        System.out.println("|         3. Buscar un producto por ID.                   |");
+        System.out.println("|         0. Salir del programa.                          |");
+        System.out.println("-----------------------------------------------------------");
     }
 }
