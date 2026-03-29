914a89f48a283b008cf1a867f54d01baec9dc04a
andres <saldaandres@hotmail.es>
2026-03-28 21:06:15 -0400

    Se implementa registrarProducto y mostrarInventario

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,45 @@
 import java.util.Scanner;
 
 public class Main {
+    Scanner scanner;
+    ArbolInventario arbol;
+
+    public Main() {
+        scanner = new Scanner(System.in);
+        arbol = new ArbolInventario();
+    }
+
     static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Main menu = new Main();
         System.out.println("\n              Bienvenido al Sistema de Control de Inventario");
         int eleccion = 1;
         while (eleccion != 0) {
-            imprimirMenu();
+            menu.imprimirMenu();
             System.out.print("\nDigita tu eleccion aqui: ");
-            eleccion = scanner.nextInt();
+            eleccion = menu.scanner.nextInt();
+            switch (eleccion) {
+                case 1: menu.registrarProducto();
+                    break;
+                case 2: menu.mostrarInventario();
+            }
         }
     }
 
-    public static void imprimirMenu() {
+    void registrarProducto() {
+        System.out.print("Digita el ID del nuevo producto: ");
+        int id = scanner.nextInt();
+        System.out.print("Digita el Nombre del nuevo producto: ");
+        String nombre = scanner.next();
+        arbol.registrarProducto(id, nombre);
+        System.out.println("Producto anadido con exito");
+    }
+
+    void mostrarInventario() {
+        System.out.println("\n---------- Inventario Actual -----------");
+        arbol.recorrerEnOrden();
+    }
+
+    void imprimirMenu() {
         System.out.println("------- Escoge una de las opciones a continuacion -------");
         System.out.println("|         1. Registrar nuevo producto.                    |");
         System.out.println("|         2. Mostrar el inventario.                       |");
